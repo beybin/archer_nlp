981c16e645d68eb9c646bcb801f14cae3dc8ae8a
beybin <1092386160@qq.com>
2022-04-11 11:47:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup, find_packages
 setup(
     name='pdutils',  # 应用名
     version='0.1.0',  # 版本号
-    # packages=['pdutils'],  # 包括在安装包内的 Python 包
-    packages=find_packages(),
+    description='utils',
+    packages=['pdutils'],  # 包括在安装包内的 Python 包
+    # packages=find_packages(),
     install_requires=["pandas", "numpy"]
 )
