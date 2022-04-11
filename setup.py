c13b075eb20e7408c5c8b59dac7bdea9c7abc5c9
1092386160@qq.com <xbb1222@_*>
2022-04-11 15:57:58 +0800

    更新项目结构。

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archer_nlp',
-    version='0.1.2',
+    version='0.1.3',
     description='archer nlp',
     long_description='',
     license='Apache License 2.0',
