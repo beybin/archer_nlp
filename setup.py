953fb7690b9f0388c8f2c5d552034f26ba140b12
1092386160@qq.com <xbb1222@_*>
2022-04-15 22:08:23 +0800

    优化write_excel函数。

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archer_nlp',
-    version='0.1.6',
+    version='0.1.7',
     description='archer nlp',
     long_description='',
     license='Apache License 2.0',
